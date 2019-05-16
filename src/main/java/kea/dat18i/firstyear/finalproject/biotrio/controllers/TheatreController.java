f93b464ca6b9f06982fed4865635d4b514e2c8b7
Aneja Orlic <aneja.orlic@hotmail.com>
2019-05-16 11:41:13 +0200

    errors fixed

--- src/main/java/kea/dat18i/firstyear/finalproject/biotrio/controllers/TheatreController.java
+++ src/main/java/kea/dat18i/firstyear/finalproject/biotrio/controllers/TheatreController.java
@@ -73,7 +73,7 @@ public class TheatreController {
     // Edit theatre from MySQL database and redirect back to /theatres
     @PostMapping(value = "/theatres/edit/{index}")
     public String handleEditTheatre(@PathVariable int index, @ModelAttribute Theatre theatre) {
-        theatreRepo.editTheatre(theatre, theatreList.get(index).getId());
+        theatreRepo.editTheatre(theatre, theatreList.get(index).getTheatre_id());
         return "redirect:/theatres";
     }
 
