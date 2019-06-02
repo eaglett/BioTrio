9d0b6527bdb2d6177f27385e15d8bf74a2d8bb55
MadaPas <mada0193@stud.kea.dk>
2019-06-02 22:09:43 +0200

    javadocs for entities

--- src/main/java/kea/dat18i/firstyear/finalproject/biotrio/entities/Admin.java
+++
@@ -1,8 +0,0 @@
-package kea.dat18i.firstyear.finalproject.biotrio.entities;
-
-
-// dunno if we actually need this since an Admin is an employee so the
-// Employee class should cover their name, password, and username
-public class Admin extends Employee {
-
-}
--- src/main/java/kea/dat18i/firstyear/finalproject/biotrio/entities/Employee.java
+++ src/main/java/kea/dat18i/firstyear/finalproject/biotrio/entities/Employee.java
@@ -1,10 +1,10 @@
 package kea.dat18i.firstyear.finalproject.biotrio.entities;
 
+/**
+ *  An employee of Bio Trio who has the authority to CRUD shows as well as book tickets
+ *  for customers who wish to book their ticket over the phone.
+ */
 
-// An employee of Bio Trio who has the authority to CRUD shows as well as book tickets
-// for customers who wish to book their ticket over the phone.
-// The administrator can also be considered as an "employee" of Bio Trio,
-// hence this class being used to classify the "Admin" actor
 public class Employee extends User {
 
     private String username;
